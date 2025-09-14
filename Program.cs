776f90803e39ab8cf1a3677668fa0d4d9eb15196
TarekAkkawi89 <149930287+TarekAkkawi89@users.noreply.github.com>
2025-09-14 20:11:12 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,7 +15,37 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Datacontext>(options => options.UseSqlite("Data Source=app.db;Cache=Shared"));
 builder.Services.AddScoped<INotesRepository, NotesRepository>();
 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Notes API", Version = "v1" });
 
+    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "API Key needed to access the endpoints. Example: \"3EAHealth\"",
+        Name = "Authorization",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.ApiKey,
+        Scheme = "ApiKeyScheme"
+    });
+
+    c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+            {
+                Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                {
+                    Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                    Id = "ApiKey"
+                },
+                Scheme = "ApiKeyScheme",
+                Name = "Authorization",
+                In = Microsoft.OpenApi.Models.ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
