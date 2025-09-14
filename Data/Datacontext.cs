1b4330021d07a7ff7a9bc211a30c276536fc02b5
TarekAkkawi89 <149930287+TarekAkkawi89@users.noreply.github.com>
2025-09-14 19:45:54 +0300

    Add files via upload

---
+++ Data/Datacontext.cs
@@ -0,0 +1,15 @@
+﻿using _3EA_Health.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace _3EA_Health.Data
+{
+    public class Datacontext : DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) :base(options)
+        {
+            
+        }    
+
+        public DbSet<Notes> Notes => Set<Notes>();
+    }
+}
